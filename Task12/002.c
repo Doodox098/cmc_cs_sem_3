748632c3f43f6cdc0d9906fbd932f3519dbf73b1
Doodox098 <s02210008@gse.cs.msu.ru>
2022-11-17 15:50:10 +0300

    12-2

--- Task12/002.c
+++ Task12/002.c
@@ -1,59 +1,30 @@
-#define _GNU_SOURCE
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <errno.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+int
+cmd(char *cur_cmd)
 {
-    int or;
-    int ans = 0;
-    int pid = fork();
+    int pid;
+    pid = fork();
     if (pid < 0) {
         return 0;
     }
     if (!pid) {
-        execlp(argv[1], argv[1], NULL);
-        _exit(1);
-    }
-    int pstatus;
-    wait(&pstatus);
-    if (!(WIFEXITED(pstatus) && WEXITSTATUS(pstatus) == 0)) {
-        pid = fork();
-        if (pid < 0) {
-            return 0;
-        }
-        if (!pid) {
-            execlp(argv[2], argv[2], NULL);
-            _exit(1);
-        }
-        wait(&pstatus);
-        if (!(WIFEXITED(pstatus) && WEXITSTATUS(pstatus) == 0)) {
-            or = 0;
-        } else {
-            or = 1;
-        }
+        execlp(cur_cmd, cur_cmd, NULL);
+        return 1;
     } else {
-        or = 1;
+        int status;
+        wait(&status);
+        return !WEXITSTATUS(status) && WIFEXITED(status);
     }
-    if (or) {
-        pid = fork();
-        if (pid < 0) {
-            return 0;
-        }
-        if (!pid) {
-            execlp(argv[3], argv[3], NULL);
-            _exit(1);
-        }
-        wait(&pstatus);
-        if (WIFEXITED(pstatus) && WEXITSTATUS(pstatus) == 0) {
-            ans = 1;
-        }
-    } else {
-        ans = 0;
-    }
-    exit(ans);
+}
+
+int
+main(int argc, char **argv)
+{
+    int ans;
+    ans = !((cmd(argv[1]) || cmd(argv[2])) && cmd(argv[3]));
+    return ans;
 }
